a9843c2e02a064ed141d5b6820d2983cdbe7df40
SAHIBZADA-DANY <angelgaming331@gmail.com>
2026-01-20 13:11:36 +0500

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -1,3 +1,13 @@
 public class patterns {
-    
+    public static void main(String [] args){
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8-1; j++){
+                System.out.print(" ");
+            }
+            for(int z = 0; z <=i; z++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
