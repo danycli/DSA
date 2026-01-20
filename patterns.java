0244eeadfb42b903ee5b158b6b52cb064543c08f
SAHIBZADA-DANY <angelgaming331@gmail.com>
2026-01-20 17:22:43 +0500

    diamond

--- patterns.java
+++ patterns.java
@@ -1,11 +1,20 @@
 public class patterns {
     public static void main(String [] args){
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8-1; j++){
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4-i; j++){
                 System.out.print(" ");
             }
-            for(int z = 0; z <=i; z++){
-                System.out.print("*");
+            for(int z = 0; z < i; z++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int o = 4; o > 0; o--){
+            for(int h = 0; h < 4-o; h++){
+                System.out.print(" ");
+            }
+            for(int e = 0; e < o; e++){
+                System.out.print("* ");
             }
             System.out.println();
         }
