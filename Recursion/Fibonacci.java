14c925adb5311d550c6c6e8d63cc483def8a11f7
danycli <angelgaming331@gmail.com>
2026-02-02 16:06:57 +0500

    rec

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -2,13 +2,17 @@ package Recursion;
 
 public class Fibonacci {
     public static int fib(int n){
-        if(n <= 2){
+        if(n == 0){
+            return 0;
+        }else if(n <= 2){
             return 1;
         }
         int fib1 = fib(n-1) + fib(n-2);
         return fib1;
     }
     public static void main(String [] args){
-        System.out.println(fib(6));
+        for(int i = 0; i < 6; i++){
+            System.out.println(fib(i));
+        }
     }
 }
