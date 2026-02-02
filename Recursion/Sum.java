cc15570c18c41f9541bc1fe5e92479f3dbdd831f
danycli <angelgaming331@gmail.com>
2026-02-02 15:49:45 +0500

    rec

--- Recursion/Sum.java
+++ Recursion/Sum.java
@@ -1,15 +1,15 @@
 package Recursion;
 
-public class Fibonacci {
-    public static void fib(int x, int sum){
+public class Sum {
+    public static void sum(int x, int sum){
         if(x == 0){
             System.out.println(sum);
             return;
         }
         sum+=x;
-        fib(x-1,sum);
+        sum(x-1,sum);
     }
     public static void main(String [] args){
-        fib(6,0);
+        sum(6,0);
     }
 }
